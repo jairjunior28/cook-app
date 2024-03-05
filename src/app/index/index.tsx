516b37c167fac9966e8ebd1a0c0151adaaed3d6c
jairjunior28 <jairjunior242011@gmail.com>
2024-03-05 15:13:54 -0300

    commit

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,14 +1,11 @@
-import {View, Text, Button} from "react-native"
+import {View, Text} from "react-native"
 import { styles } from "./styles"
 
 export default function Index(){
     return(
         <View style={styles.container}>
-            <Text style={styles.texto}>Teste</Text>
-            <Button title="Saiba mais"
-            onPress={()=>{
-                alert('Ola clickou no botao')
-            }}/>
+            <Text> Teste </Text>
+          
         </View>
 
     )
--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -1,15 +1,7 @@
 import { StyleSheet } from "react-native"
 
 export const styles =StyleSheet.create({
-    container:{
+    container: {
         backgroundColor: 'red',
-        height: 100,
-        
-
-    },
-    texto:{
-        textAlign: 'center',
-        fontSize: 100,
-        color: 'white'
     }
 })
