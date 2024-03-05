f147293d7e156ce965a62a3909d780e836057b22
jairjunior28 <jairjunior242011@gmail.com>
2024-03-05 12:00:03 -0300

    testes brincando com react familiarizando novamene=te

--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -2,13 +2,14 @@ import { StyleSheet } from "react-native"
 
 export const styles =StyleSheet.create({
     container:{
-        backgroundColor: 'pink',
-        fontFamily: 'arial',
-        height: 80,
+        backgroundColor: 'red',
+        height: 100,
         
 
     },
     texto:{
-        textAlign: 'center'
+        textAlign: 'center',
+        fontSize: 100,
+        color: 'white'
     }
 })
