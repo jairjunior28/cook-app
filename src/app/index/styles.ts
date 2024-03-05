3a8e8e5fc715041eace88156b6a43f762f45b360
jairjunior28 <jairjunior242011@gmail.com>
2024-03-05 11:13:15 -0300

    first commit

---
+++ src/app/index/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+
+export const styles =StyleSheet.create({
+    container:{
+        backgroundColor: 'pink',
+        fontFamily: 'arial',
+        height: 80,
+        
+
+    },
+    texto:{
+        textAlign: 'center'
+    }
+})
